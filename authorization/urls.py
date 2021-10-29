86bd6b8a402f7c4cc63cff33c86014f5a2d2453c
dima203 <gg37500@yandex.by>
2021-10-29 23:06:17 +0300

    Fix layout

--- authorization/urls.py
+++ authorization/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
     path('about', cache_page(15 * 60)(views.about_page), name='about'),
     path('articles', views.ArticlesPage.as_view(), name='articles'),
     path('articles/create', views.ArticleCreateFormView.as_view(), name='article_create'),
-    path('articles/<int:article_id>', views.ArticleDetailsView.as_view(), name='article_page'),
+    path('articles/<int:article_id>', cache_page(60)(views.ArticleDetailsView.as_view()), name='article_page'),
     path('articles/<int:article_id>/update', views.ArticleUpdate.as_view(), name='article_update'),
     path('articles/<int:article_id>/delete', views.ArticleDelete.as_view(), name='article_delete'),
 ]
