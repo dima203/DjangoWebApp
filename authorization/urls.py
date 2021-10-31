37867f3eebe9bb2eaa4d58a234f73e0e4792680a
dima203 <gg37500@yandex.by>
2021-10-31 20:09:05 +0300

    Cache fix

--- authorization/urls.py
+++ authorization/urls.py
@@ -4,14 +4,14 @@ from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    path('', cache_page(1)(views.home_page), name='home'),
-    path('user/<int:user_id>', cache_page(1)(views.UserDetailsView.as_view()), name='user_page'),
+    path('', cache_page(15 * 60)(views.home_page), name='home'),
+    path('user/<int:user_id>', cache_page(5 * 60)(views.UserDetailsView.as_view()), name='user_page'),
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/registration', views.RegistrationForm.as_view(), name='registration'),
-    path('about', cache_page(1)(views.about_page), name='about'),
+    path('about', cache_page(15 * 60)(views.about_page), name='about'),
     path('articles', views.ArticlesPage.as_view(), name='articles'),
     path('articles/create', views.ArticleCreateFormView.as_view(), name='article_create'),
-    path('articles/<int:article_id>', cache_page(1)(views.ArticleDetailsView.as_view()), name='article_page'),
+    path('articles/<int:article_id>', cache_page(60)(views.ArticleDetailsView.as_view()), name='article_page'),
     path('articles/<int:article_id>/update', views.ArticleUpdate.as_view(), name='article_update'),
     path('articles/<int:article_id>/delete', views.ArticleDelete.as_view(), name='article_delete'),
 ]
