a623d9828f2db60f8a6644350811e957cfda5fb6
dima203 <gg37500@yandex.by>
2021-11-18 18:35:39 +0300

    Docker

--- Dockerfile
+++ Dockerfile
@@ -4,4 +4,5 @@ ENV PYTHONUNBUFFERED=1
 WORKDIR /code
 COPY requirements.txt /code/
 RUN pip install -r requirements.txt
+RUN python manage.py collectstatic --noinput
 COPY . /code/
--- webapp/settings.py
+++ webapp/settings.py
@@ -135,6 +135,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
