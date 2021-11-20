e8c24e6d2049ff6c248b6e6d5ef74781b6310ce5
dima203 <gg37500@yandex.by>
2021-11-20 15:23:44 +0300

    Docker

--- webapp/settings.py
+++ webapp/settings.py
@@ -148,10 +148,6 @@ STATICFILES_DIRS = [
 
 LOGIN_REDIRECT_URL = '/'
 
-import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
-
 CORS_ALLOW_ALL_ORIGINS = True
 
 INTERNAL_IPS = [
