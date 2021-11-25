72ab6a53e0e6fbdb09fedf2d105baabfe59da904
dima203 <gg37500@yandex.by>
2021-11-25 17:09:05 +0300

    Fix bug

--- authorization/urls.py
+++ authorization/urls.py
@@ -4,11 +4,11 @@ from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    path('', cache_page(15 * 60)(views.home_page), name='home'),
+    path('', views.home_page, name='home'),
     path('user/<int:user_id>', cache_page(5 * 60)(views.UserDetailsView.as_view()), name='user_page'),
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/registration', views.RegistrationForm.as_view(), name='registration'),
-    path('about', cache_page(15 * 60)(views.about_page), name='about'),
+    path('about', views.about_page, name='about'),
     path('articles', views.ArticlesPage.as_view(), name='articles'),
     path('articles/create', views.ArticleCreateFormView.as_view(), name='article_create'),
     path('articles/<int:article_id>', cache_page(60)(views.ArticleDetailsView.as_view()), name='article_page'),
