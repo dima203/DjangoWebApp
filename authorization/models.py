db99cf5629280b062d2c88cd00b65d8e7ea7407e
dima203 <gg37500@yandex.by>
2022-09-10 21:32:09 +0300

    migrations

--- authorization/models.py
+++ authorization/models.py
@@ -4,8 +4,8 @@ from django.db import models
 class Article(models.Model):
     title = models.CharField(max_length=256, verbose_name='Заголовок')
     content = models.TextField(verbose_name='Текст статьи')
-    next_article = models.URLField(verbose_name="Следующая статья")
-    prev_article = models.URLField(verbose_name="Предыдущая статья")
+    next_article = models.URLField(verbose_name="Следующая статья", default="#")
+    prev_article = models.URLField(verbose_name="Предыдущая статья", default="#")
     date_created = models.DateTimeField(auto_now_add=True, verbose_name='Дата и время публикации')
     author = models.CharField(max_length=256, verbose_name='Автор')
 
